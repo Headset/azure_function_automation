db01599df5b713e2476fb3139a1985779ca8644a
Mitchell Laferla <mlaferla@headset.io>
2023-06-01 12:55:04 -0500

    changed file structure

--- test_file/test_py_one.py
+++ test_file/test_py_one.py
@@ -6,15 +6,6 @@ import datetime
 import logging
 
 
-def main(mytimer: func.TimerRequest) -> None:
-    utc_timestamp = datetime.datetime.utcnow().replace(
-        tzinfo=datetime.timezone.utc).isoformat()
-
-    if mytimer.past_due:
-        logging.info('The timer is past due!')
-
-    logging.info('Python timer trigger function ran at %s', utc_timestamp)
-
 #add snowflake.sqlachelmy depependencies
 registry.register('snowflake', 'snowflake.sqlalchemy', 'dialect')
 
