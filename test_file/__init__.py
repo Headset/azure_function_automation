22aa39dc4b9896d428456ca410447cec86678937
Mitchell Laferla <mlaferla@headset.io>
2023-06-01 16:48:33 -0500

    attempting subprocess

--- test_file/__init__.py
+++ test_file/__init__.py
@@ -4,6 +4,7 @@ from sqlalchemy.dialects import registry
 import os
 import datetime
 import logging
+import subprocess
 
 
 def main(mytimer: func.TimerRequest) -> None:
@@ -14,3 +15,14 @@ def main(mytimer: func.TimerRequest) -> None:
         logging.info('The timer is past due!')
 
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
+
+
+filepaths = ["C:\\Users\\harsh\\My Drive\\Folder\\Code\\3.py",
+    "C:\\Users\\harsh\\My Drive\\Folder\\Code\\4.py",
+    "C:\\Users\\harsh\\My Drive\\Folder\\Code\\5.py",
+    "C:\\Users\\harsh\\My Drive\\Folder\\Code\\Helper codes\\6.py",
+    "C:\\Users\\harsh\\My Drive\\Folder\\Code\\Process\\7.py"
+]
+
+for filepath in filepaths:
+    subprocess.call(["python", filepath])
