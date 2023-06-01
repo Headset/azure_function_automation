4050ab4a8ee67fb7c4599ec97785ac11b91f71e9
Mitchell Laferla <mlaferla@headset.io>
2023-06-01 12:25:45 -0500

    file restructure

---
+++ test_file/test_py_one.py
@@ -0,0 +1,62 @@
+import azure.functions as func
+from sqlalchemy import create_engine
+from sqlalchemy.dialects import registry
+import os
+import datetime
+import logging
+
+
+def main(mytimer: func.TimerRequest) -> None:
+    utc_timestamp = datetime.datetime.utcnow().replace(
+        tzinfo=datetime.timezone.utc).isoformat()
+
+    if mytimer.past_due:
+        logging.info('The timer is past due!')
+
+    logging.info('Python timer trigger function ran at %s', utc_timestamp)
+
+#add snowflake.sqlachelmy depependencies
+registry.register('snowflake', 'snowflake.sqlalchemy', 'dialect')
+
+# Create connection to snowflake db
+engine = create_engine(
+    'snowflake://{user}:{password}@{account}/'.format(
+        user=os.environ["SNOWFLAKE_USER"],
+        password=os.environ["SNOWFLAKE_PASSWORD"],
+        account=os.environ["SNOWFLAKE_ACCOUNT"],
+    ))
+
+
+# set environment
+usewh_string = "use warehouse ANALYST_WH;"
+usedb_string = "use FIVETRAN;"
+userole_string = "use role etl_user"
+
+
+
+# truncate table 
+sql_string_0 = """
+
+Insert into lizland.dukeofne.azure_function_one
+values
+(
+'azure function one'
+, current_date
+, current_timestamp
+)
+
+"""
+
+
+# create connection to DB and tell to use Fivetran
+connection = engine.connect()
+connection.execute(usewh_string)
+connection.execute(usedb_string)
+connection.execute(userole_string)
+
+connection.execute(sql_string_0)
+
+
+# close connection
+connection.close()
+engine.dispose()
