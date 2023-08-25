10b803c5290d944006c2d8e5a32a57404de22c84
Mitchell Laferla <mlaferla@headset.io>
2023-08-25 11:17:07 -0700

    debugging

--- monitoring_daily/function_app.py
+++ monitoring_daily/function_app.py
@@ -6,6 +6,14 @@ import sys
 import importlib.util
 
 
+# Azure function timer. To alter timer cadence see function.json file
+app = func.FunctionApp()
+
+@app.function_name(name="monitoring_daily")
+@app.schedule(schedule="0 5 * * MON", 
+              arg_name="mytimer",
+              run_on_startup=False,
+              use_monitor=True) 
 
 def main(mytimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
@@ -13,5 +21,5 @@ def main(mytimer: func.TimerRequest) -> None:
     
     if mytimer.past_due:
         logging.info('The timer is past due!')
-
+        
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
--- update_state_totals/function_app.py
+++ update_state_totals/function_app.py
@@ -5,7 +5,23 @@ import logging
 import sys
 import importlib.util
 
+# Azure function timer. To alter timer cadence see function.json file
+app = func.FunctionApp()
 
+@app.function_name(name="update_state_totals")
+@app.schedule(schedule="0 5 * * MON", 
+              arg_name="mytimer",
+              run_on_startup=False,
+              use_monitor=True) 
+
+def main(mytimer: func.TimerRequest) -> None:
+    utc_timestamp = datetime.datetime.utcnow().replace(
+        tzinfo=datetime.timezone.utc).isoformat()
+    
+    if mytimer.past_due:
+        logging.info('The timer is past due!')
+        
+    logging.info('Python timer trigger function ran at %s', utc_timestamp)
 
 def main(mytimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
