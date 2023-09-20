122941913731f405483fcf81bd3bd8230af57f6b
Mitchell Laferla <mlaferla@headset.io>
2023-09-19 17:00:21 -0700

    ...

--- monitoring_weekly/function_app.py
+++ monitoring_weekly/function_app.py
@@ -1,21 +1,18 @@
-import azure.functions as func
-import logging
-
-
-# Azure function timer. To alter timer cadence see function.json file
+import  azure.functions  as  func
+import  logging
+import  datetime
+  
 app = func.FunctionApp()
 
-@app.function_name(name="monitoring_weekly")
-@app.schedule(schedule="0 5 * * 1", 
-              arg_name="mytimer",
-              run_on_startup=True,
-              use_monitor=True) 
+@app.function_name(name="mytimer")
+@app.schedule(schedule="0 */5 * * * *", arg_name="mytimer", run_on_startup=True,
+use_monitor=True)
 
-def main(mytimer: func.TimerRequest) -> None:
+def  test_function(mytimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
-        tzinfo=datetime.timezone.utc).isoformat()
-    
-    if mytimer.past_due:
+    tzinfo=datetime.timezone.utc).isoformat()
+
+
+    if  mytimer.past_due:
         logging.info('The timer is past due!')
-        
-    logging.info('Python timer trigger function ran at %s', utc_timestamp)
+        logging.info('Python timer trigger function ran at %s', utc_timestamp)
